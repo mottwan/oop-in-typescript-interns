44fb186c6455084a62881c28ab33313554f2785d
Ivan Motelica <motelica.ion@gmail.com>
2024-08-15 11:26:01 +0300

    implement abstract getPageTitle() method

--- src/pages/dashboard-page.ts
+++ src/pages/dashboard-page.ts
@@ -2,15 +2,19 @@ import {BasePage} from "./base-page";
 import {By} from "selenium-webdriver";
 
 export class DashboardPage extends BasePage {
-    private static dashboardPageURL: string = '/dashboard';
+    private static dashboardPageURL: string = '/secure';
     private welcomeMessageSelector: By = By.id('welcomeMessage');
 
     async open(path: string = DashboardPage.dashboardPageURL) {
-        return await this.driver.get(path);
+        return await this.driver.get(this.baseUrl + path);
     }
 
 
     async isWelcomeMessageDisplayed() {
         return await this.driver.findElement(this.welcomeMessageSelector).isDisplayed()
+    }
+
+    async getPageTitle(): Promise<string> {
+        return await this.driver.getTitle();
     }
 }
