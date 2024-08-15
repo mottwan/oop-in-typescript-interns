3341ac879794d631232c1533aca875685aee7ae1
Ivan Motelica <motelica.ion@gmail.com>
2024-08-15 11:21:40 +0300

    Update base-page.ts

--- src/pages/base-page.ts
+++ src/pages/base-page.ts
@@ -2,6 +2,7 @@ import { WebDriver } from 'selenium-webdriver';
 
 export abstract class BasePage {
     protected driver: WebDriver;
+    protected baseUrl: string = 'http://the-internet.herokuapp.com';
 
     constructor(driver: WebDriver) {
         this.driver = driver;
@@ -9,5 +10,6 @@ export abstract class BasePage {
 
     abstract open(path: string): Promise<void>;
 
+    abstract getPageTitle(): Promise<void>;
 
 }
